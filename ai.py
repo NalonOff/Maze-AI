d4ca1866e6e936e4a880d5ee95145dbe4945d38c
NalonOff <nalon.projects@gmail.com>
2023-11-24 18:58:37 +0100

    Ajout de la trainee du personnage

--- ai.py
+++ ai.py
@@ -5,3 +5,5 @@ class AI:
         self.image = pg.transform.scale2x(pg.image.load('assets\\textures\\player.png'))
         self.pos = [32, 0]
         self.trail = []
+
+        self.learningRate = 0.001
